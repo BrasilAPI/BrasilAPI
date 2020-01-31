3959aafca6984ba364debdc248509fe9b3dd9e46
lucianopf <lucianopf@outlook.com>
2020-01-30 21:18:55 -0300

    cache: set max-age and s-max-age

--- pages/api/cep/[cep].js
+++ pages/api/cep/[cep].js
@@ -3,6 +3,8 @@ import cep from 'cep-promise';
 export default async function Cep(request, response) {
     const requestedCep = request.query.cep;
 
+    response.setHeader('Cache-Control', 'max-age=0, s-maxage=86400');
+
     try {
         const cepResult = await cep(requestedCep);
 
