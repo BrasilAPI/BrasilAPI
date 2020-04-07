997c44f77482e930b81b344bb22f2f79699e8649
Weslley Rocha <weslleytato.12@gmail.com>
2020-04-07 14:58:00 -0300

    fix: moved graphql to modules

--- pages/api/graphql/v1/index.js
+++ pages/api/graphql/v1/index.js
@@ -1,52 +1,25 @@
-import { ApolloServer, gql, makeExecutableSchema } from 'apollo-server-micro';
-import { merge } from 'lodash';
-import {
-    typeDefs as CEPTypeDefs,
-    resolvers as CEPResolvers
-} from '../../cep/v1/[cep]';
-import {
-    typeDefs as StatusTypeDefs,
-    resolvers as StatusResolvers
-} from '../../status/v1';
-
-import Cors from 'micro-cors';
+import { ApolloServer } from "apollo-server-micro";
+import Cors from "micro-cors";
+import { schema } from "../../../../graphql/schema";
 
 const cors = Cors({
-    allowMethods: ['POST', 'OPTIONS']
-});
-
-const RootTypeDefs = gql`
-    type Query {
-        """
-        query vazia como um workaround na escalabilidade de cada endpoint definir seu proprio type e extender a RootQuery
-        """
-        _empty: String
-    }
-`;
-
-const resolvers = {
-    Query: {}
-};
-
-const schema = makeExecutableSchema({
-    typeDefs: [RootTypeDefs, CEPTypeDefs, StatusTypeDefs],
-    resolvers: merge(resolvers, StatusResolvers, CEPResolvers)
+  allowMethods: ["POST", "OPTIONS"],
 });
 
 const apolloServer = new ApolloServer({
-    schema,
-    introspection: true,
-    context: ({ req, res }) => {
-        return { req, res };
-    }
+  schema,
+  introspection: true,
+  context: ({ req, res }) => {
+    return { req, res };
+  },
 });
 
-const handler = apolloServer.createHandler({ path: '/api/graphql/v1' });
+const handler = apolloServer.createHandler({ path: "/api/graphql/v1" });
 
 export const config = {
-    api: {
-        bodyParser: false
-    }
+  api: {
+    bodyParser: false,
+  },
 };
 
 export default cors(handler);
