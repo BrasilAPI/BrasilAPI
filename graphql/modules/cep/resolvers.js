90c0d67ac9a8d9dd9a802348509cc7cd412a1735
Weslley Rocha <weslleytato.12@gmail.com>
2020-04-07 14:58:00 -0300

    fix: moved graphql to modules

--- graphql/modules/cep/resolvers.js
+++ graphql/modules/cep/resolvers.js
@@ -1,5 +1,5 @@
-import microCors from 'micro-cors';
-import cep from 'cep-promise';
+import { ApolloError } from "apollo-server-micro";
+import cep from "cep-promise";
 
 // max-age especifica quanto tempo o browser deve manter o valor em cache, em segundos.
 // s-maxage é uma header lida pelo servidor proxy (neste caso, o Now da ZEIT).
@@ -17,30 +17,24 @@ import cep from 'cep-promise';
 //    sem necessidade, só ocupando espaço em disco. A história seria diferente se a API
 //    servisse fotos dos usuários, por exemplo. Além disso teríamos problemas com
 //    stale/out-of-date cache caso alterássemos a implementação da API.
-const CACHE_CONTROL_HEADER_VALUE = 'max-age=0, s-maxage=86400, stale-while-revalidate, public';
-const cors = microCors();
+const CACHE_CONTROL_HEADER_VALUE =
+  "max-age=0, s-maxage=86400, stale-while-revalidate, public";
 
-async function Cep(request, response) {
-    const requestedCep = request.query.cep;
+export default {
+  Query: {
+    cep: async (_parent, _args, _context) => {
+      if (_args.cep.length !== 8) {
+        throw new ApolloError("CEP inválido", "validation_error");
+      }
 
-    response.setHeader('Cache-Control', CACHE_CONTROL_HEADER_VALUE);
-
-    try {
-        const cepResult = await cep(requestedCep);
-
-        response.status(200);
-        response.json(cepResult);
-
-    } catch (error) {
-        if (error.name === 'CepPromiseError') {
-            response.status(404);
-            response.json(error);
-            return;
-        }
-
-        response.status(500);
-        response.json(error);
-    }
-}
-
-export default cors(Cep);
+      try {
+        console.log(_context.res.setHeader);
+        _context.res.setHeader("Cache-Control", CACHE_CONTROL_HEADER_VALUE);
+        const cepResult = await cep(_args.cep);
+        return cepResult;
+      } catch (err) {
+        throw new ApolloError("Erro ao consultar CEP", err.type, err.errors);
+      }
+    },
+  },
+};
