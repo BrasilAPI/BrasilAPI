041d220c201b44f1362f310a5f360df145151fdd
Paulo Santana <paulo.santana.r@gmail.com>
2020-04-20 03:07:40 -0300

    fix(tests): fix jest not exiting after done testing

--- tests/helpers/server.js
+++ tests/helpers/server.js
@@ -1,6 +1,6 @@
 const waitOn = require('wait-on');
 const child_process = require('child_process');
-const environment = process.env.NODE_ENV || 'test';
+// const environment = process.env.NODE_ENV || 'test';
 
 function createServer() {
     let localServerProcess;
@@ -8,29 +8,39 @@ function createServer() {
     function start() {
         const statusUrl = `${getUrl()}/api/status/v1`;
 
-        if (environment === 'test') {
-            localServerProcess = startLocalServer();
-        }
-        
+        // if (environment === 'test') {
+        //     localServerProcess = startLocalServer();
+        // }
+        localServerProcess = startLocalServer();
+
         return waitOn({
             resources: [statusUrl]
         });
     }
 
     function startLocalServer() {
-        return child_process.exec('npm run dev');
+        if(process.platform === 'win32') {
+            return child_process.spawn('npm.cmd', ['run', 'dev']);
+        } else {
+            return child_process.spawn('node', ['node_modules/.bin/next']);
+        }
     }
 
     function stop() {
         if(localServerProcess) {
-            localServerProcess.kill('SIGINT');
+            if(process.platform === 'win32') {
+                child_process.execSync(`taskkill /F /T /PID ${localServerProcess.pid}`);
+            } else {
+                localServerProcess.kill('SIGINT');
+
+            }
         }
     }
 
     function getUrl() {
-        if (process.env.NODE_ENV === 'ci') {
-            return process.env.PREVIEW_URL;
-        }
+        // if (process.env.NODE_ENV === 'ci') {
+        //     return process.env.PREVIEW_URL;
+        // }
         
         return 'http://localhost:3000';
     }
