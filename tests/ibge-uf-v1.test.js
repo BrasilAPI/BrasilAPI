24039cb6d401e71aea93107ab05c0cadbc0ba0fe
eklauberg <duduklauberg@gmail.com>
2020-07-17 21:05:32 -0300

    adiciona ibge-uf teste

---
+++ tests/ibge-uf-v1.test.js
@@ -0,0 +1,62 @@
+const axios = require('axios');
+
+const createServer = require('./helpers/server.js');
+
+const server = createServer();
+
+beforeAll(async () => {
+  await server.start();
+});
+
+afterAll(async () => {
+  await server.stop();
+});
+
+describe('/ibge/uf/v1 (E2E)', () => {
+  test('Utilizando um Codigo válido: 22', async () => {
+    const requestUrl = `${server.getUrl()}/api/ibge/uf/v1/22`;
+    const response = await axios.get(requestUrl);
+
+    expect(response.status).toBe(200);
+    expect(response.data).toEqual({
+      id: expect.any(Number),
+      sigla: expect.any(String),
+      nome: expect.any(String),
+      regiao: expect.objectContaining({
+        id: expect.any(Number),
+        sigla: expect.any(String),
+        nome: expect.any(String),
+      }),
+    });
+  });
+
+  test('Utilizando um Codigo inexistente ou inválido: 99', async () => {
+    const requestUrl = `${server.getUrl()}/api/ibge/uf/v1/99`;
+
+    const response = await axios.get(requestUrl);
+
+    expect(response.status).toBe(200);
+    expect(response.data).toEqual([]);
+  });
+
+  test('Buscando todas as ufs', async () => {
+    const requestUrl = `${server.getUrl()}/api/ibge/uf/v1`;
+    const response = await axios.get(requestUrl);
+
+    expect(response.status).toBe(200);
+    expect(response.data).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          id: expect.any(Number),
+          sigla: expect.any(String),
+          nome: expect.any(String),
+          regiao: expect.objectContaining({
+            id: expect.any(Number),
+            sigla: expect.any(String),
+            nome: expect.any(String),
+          }),
+        }),
+      ])
+    );
+  });
+});
