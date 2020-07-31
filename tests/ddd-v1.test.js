7fe6f84e311e6c76894206c466a8d53263b10674
Otacilio Lacerda <otacilio.lacerda@zalando.de>
2020-07-31 17:15:39 +0200

    style: apply lint rules

--- tests/ddd-v1.test.js
+++ tests/ddd-v1.test.js
@@ -1,14 +1,12 @@
 const axios = require('axios');
 
+const SCENARIO_DDD_SUCCESS = require('./helpers/scenarios/ddd/success');
+const SCENARIO_DDD_INEXISTENT = require('./helpers/scenarios/ddd/inexistent');
+const SCENARIO_DDD_INCORRECT = require('./helpers/scenarios/ddd/incorrect');
+
 const createServer = require('./helpers/server.js');
 
 const server = createServer();
-
-const scenariosDdd = {
-  sucess: require('./helpers/scenarios/ddd/success'),
-  inexistent: require('./helpers/scenarios/ddd/inexistent'),
-  incorrect: require('./helpers/scenarios/ddd/incorrect'),
-};
 
 const requestUrl = `${server.getUrl()}/api/ddd/v1`;
 
@@ -23,16 +21,16 @@ afterAll(async () => {
 describe('api/ddd/v1 (E2E)', () => {
   test('Utilizando um DDD válido: 12', async () => {
     const response = await axios.get(`${requestUrl}/12`);
-    expect(response.data).toEqual(scenariosDdd.sucess);
+    expect(response.data).toEqual(SCENARIO_DDD_SUCCESS);
   });
 
   test('Utilizando um DDD inexistente: 01', async () => {
     const response = await axios.get(`${requestUrl}/01`);
-    expect(response.data).toEqual(scenariosDdd.inexistent);
+    expect(response.data).toEqual(SCENARIO_DDD_INEXISTENT);
   });
 
   test('Utilizando um DDD inválido: 9999', async () => {
     const response = await axios.get(`${requestUrl}/9999`);
-    expect(response.data).toEqual(scenariosDdd.incorrect);
+    expect(response.data).toEqual(SCENARIO_DDD_INCORRECT);
   });
 });
