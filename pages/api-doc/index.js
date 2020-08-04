c58751285fd2d12c04fafbcf29cd69985c0bc081
carlosziegler <carlos.ziegler@gmail.com>
2020-08-04 16:21:41 +0200

    change name react component

--- pages/api-doc/index.js
+++ pages/api-doc/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { RedocStandalone } from 'redoc';
 import Documentation from './doc.json'
 
-export default function index2() {
+export default function index() {
   return (
     <div>
       <RedocStandalone
