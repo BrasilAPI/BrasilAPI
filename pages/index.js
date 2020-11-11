fb0b5713ad3a65dca3942e82a7a92b2a7a332b0d
r3nanp <renanmol87@gmail.com>
2020-11-11 15:33:22 -0300

    feat (index): add head and image from next

--- pages/index.js
+++ pages/index.js
@@ -1,57 +1,74 @@
+import Head from 'next/head';
+import Image from 'next/image';
+
 export default function Index() {
   return (
-    <main>
-      <div>
-        <a
-          className="logo"
-          href="https://github.com/filipedeschamps/BrasilAPI"
-          alt="Acessar repositório do BrasilAPI no Github"
-        >
-          <img src="/brasilapi-logo-medium.png" />
-        </a>
-      </div>
+    <>
+      <Head>
+        <title>Brasil API</title>
+      </Head>
 
-      <div>
-        <a
-          href="https://vercel.com/?utm_source=brasilapi"
-          target="_blank"
-          rel="noopener"
-        >
-          <img
-            src="/powered-by-vercel.svg"
-            width="175"
-            alt="Powered by Vercel"
-          />
-        </a>
-      </div>
+      <main>
+        <div>
+          <a
+            className="logo"
+            href="https://github.com/filipedeschamps/BrasilAPI"
+            rel="noopener noreferrer"
+          >
+            <Image
+              src="/brasilapi-logo-medium.png"
+              alt="Acessar repositório do BrasilAPI no Github"
+              width="700"
+              height="438"
+            />
+          </a>
+        </div>
 
-      <style jsx global>{`
-        * {
-          box-sizing: border-box;
-          margin: 0;
-          outline: 0;
-          padding: 0;
-        }
+        <div>
+          <a
+            href="https://vercel.com/?utm_source=brasilapi"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              src="/powered-by-vercel.svg"
+              width="175"
+              height="100"
+              alt="Powered by Vercel"
+            />
+          </a>
+        </div>
 
-        html,
-        body {
-          height: 100%;
-        }
+        <style jsx global>
+          {`
+            * {
+              box-sizing: border-box;
+              margin: 0;
+              outline: 0;
+              padding: 0;
+            }
 
-        body {
-          color: #fff;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          text-align: center;
-        }
+            html,
+            body {
+              height: 100%;
+            }
 
-        main > a {
-          color: #fff;
-          font-size: 2em;
-          text-decoration: none;
-        }
-      `}</style>
-    </main>
+            body {
+              color: #fff;
+              display: flex;
+              flex-direction: column;
+              justify-content: center;
+              text-align: center;
+            }
+
+            main > a {
+              color: #fff;
+              font-size: 2em;
+              text-decoration: none;
+            }
+          `}
+        </style>
+      </main>
+    </>
   );
 }
