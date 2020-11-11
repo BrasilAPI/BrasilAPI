2a1beafdc1437f879f10d77093c54acf91d8b197
r3nanp <renanmol87@gmail.com>
2020-11-11 15:33:48 -0300

    feat (document): create next document

---
+++ pages/_document.js
@@ -0,0 +1,19 @@
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="pt-BR">
+        <Head>
+          <meta charSet="utf-8" />
+
+          <link rel="icon" href="/brasilapi-logo-small.png" type="image/png" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
