13c7d96e192e28ed7e7984c3eb6d02e3497bb65c
Rodrigo Texeira Andreotti <ro.andriotti@gmail.com>
2020-12-15 21:10:12 -0300

    feat: add routes and tests for capitals and airport current weather state

--- services/cptec/cities.js
+++ services/cptec/cities.js
@@ -2,6 +2,10 @@ import axios from 'axios';
 import { CPTEC_URL, CITY_TEMPLATE } from './constants';
 import { transform } from 'camaro';
 
+/**
+ * Get all Brazilian cities on CPTEC database
+ * @returns {Array}
+ */
 export const getAllCitiesData = async () => {
     const citiesData = await axios.get(CPTEC_URL + 'listaCidades', {
         responseType: 'application/xml',
@@ -13,6 +17,11 @@ export const getAllCitiesData = async () => {
     return jsonData;
 }
 
+/**
+ * Search Brazilian cities by name
+ * @param {string} name 
+ * @returns {Array}
+ */
 export const getCityData = async (name) => {
     const citiesData = await axios.get(CPTEC_URL + 'listaCidades?city=' + name, {
         responseType: 'application/xml',
--- services/cptec/constants.js
+++ services/cptec/constants.js
@@ -1,6 +1,33 @@
 export const CPTEC_URL = 'http://servicos.cptec.inpe.br/XML/';
+
 export const CITY_TEMPLATE = ['cidades/cidade', {
     name: 'nome',
     state: 'uf',
     code: 'id'
-}]
+}];
+
+export const CAPITAL_TEMPLATE = ['capitais/metar', {
+    icao_code: 'codigo',
+    last_update: 'atualizacao',
+    pressure: 'pressao',
+    visibility: 'intensidade', // this property should be "visibilidade", but it comes from the webservice as "intensidade"
+    wind:   'vento_int',
+    wind_direction: 'vento_dir',
+    humidity: 'umidade',
+    weather_code: 'tempo',
+    weather_desc: 'tempo_desc',
+    temp: 'temperatura'
+}];
+
+export const AIRPORT_TEMPLATE = ['metar', {
+    icao_code: 'codigo',
+    last_update: 'atualizacao',
+    pressure: 'pressao',
+    visibility: 'visibilidade', 
+    wind:   'vento_int',
+    wind_direction: 'vento_dir',
+    humidity: 'umidade',
+    weather_code: 'tempo',
+    weather_desc: 'tempo_desc',
+    temp: 'temperatura'
+}];
