23aa1538f1f958885a2b0c1ef86eb355f81874b1
Rodrigo Texeira Andreotti <ro.andriotti@gmail.com>
2020-12-17 16:58:35 -0300

    feat: add endpoint to predictions services

--- services/cptec/weather.js
+++ services/cptec/weather.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { CPTEC_URL, CAPITAL_TEMPLATE, AIRPORT_TEMPLATE } from './constants';
+import { CPTEC_URL, CAPITAL_TEMPLATE, AIRPORT_TEMPLATE, PREDICTION_TEMPLATE, CONDITION_DESCRIPTIONS } from './constants';
 import normalizeBrazilianDate from '../util/normalizeBrazilianDate';
 import { transform } from 'camaro';
 
@@ -36,4 +36,56 @@ export const getCurrentAirportWeather = async (icaoCode) => {
     const jsonData = await transform(airportWeather.data, AIRPORT_TEMPLATE);
 
     return jsonData.length > 0 && jsonData[0].icao_code !== 'UNDEFINED' ? jsonData[0] : null;
+}
+
+/**
+ * Get weather predicion for the next {days} with a limit of 14 days
+ * @param {int} cityCode 
+ * @param {int} days 
+ * @returns {object}
+ */
+export const getPredictionWeather = async (cityCode, days) => {
+    const baseUrl = CPTEC_URL + 'cidade/';
+    let url = baseUrl;
+    if(days <=4 ) {
+        url += cityCode + '/previsao.xml';
+    } else {
+        url += '7dias/' + cityCode + '/previsao.xml';
+    } 
+
+    console.log(url);
+
+    const weatherPredictions = await axios.get(url, {
+        responseType: 'application/xml',
+        responseEncoding: 'binary'
+    });
+
+    const jsonData = await transform(weatherPredictions.data, PREDICTION_TEMPLATE);
+    if(jsonData.city_name === 'null') {
+        return null;
+    }
+
+    // If number of days requested was greater than 7, load extended data from service
+    if(days > 7) {
+        const extendedPredictions = await axios.get(baseUrl +  cityCode + '/estendida.xml', {
+            responseType: 'application/xml',
+            responseEncoding: 'binary'
+        });
+
+        
+        const extendedJsonData = await transform(extendedPredictions.data, PREDICTION_TEMPLATE);
+
+        jsonData.weather = [ ...jsonData.weather, ...extendedJsonData.weather];
+    }
+
+    // IF total data greater than requested number of days, slice array into correct size
+    if(jsonData.weather.length > days) {
+        jsonData.weather = jsonData.weather.slice(0, days);
+    }
+
+    jsonData.weather.map((pred) =>  {
+        pred.condition_desc = CONDITION_DESCRIPTIONS[pred.condition];
+    });
+
+    return jsonData;
 }
