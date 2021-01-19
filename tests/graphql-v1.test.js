a85188a31c0140eb90ce08cbf995a814ab03f7f4
Marcon Willian <marcon@mh4sh.dev>
2021-01-18 23:26:28 -0400

    test: Adicionar testes do graphql bank e separar subblocos por describe

--- tests/graphql-v1.test.js
+++ tests/graphql-v1.test.js
@@ -1,85 +1,154 @@
 const axios = require('axios');
 
 describe('/graphql/v1 (E2E)', () => {
-  test('Utilizando um CEP válido: 05010000', async () => {
-    const requestUrl = `${global.SERVER_URL}/api/graphql/v1`;
-    const response = await axios.post(requestUrl, {
-      query: `query FetchCep($cep: String!){
-                cep(cep: $cep) {
-                    cep
-                    state
-                    city
-                    street
-                    neighborhood
-                }
-            }`,
-      variables: {
+  describe('CEP', () => {
+    test('Utilizando um CEP válido: 05010000', async () => {
+      const requestUrl = `${global.SERVER_URL}/api/graphql/v1`;
+      const response = await axios.post(requestUrl, {
+        query: `query FetchCep($cep: String!){
+                  cep(cep: $cep) {
+                      cep
+                      state
+                      city
+                      street
+                      neighborhood
+                  }
+              }`,
+        variables: {
+          cep: '05010000',
+        },
+      });
+
+      expect(response.data.data.cep).toEqual({
         cep: '05010000',
-      },
+        state: 'SP',
+        city: 'São Paulo',
+        neighborhood: 'Perdizes',
+        street: 'Rua Caiubi',
+      });
     });
 
-    expect(response.data.data.cep).toEqual({
-      cep: '05010000',
-      state: 'SP',
-      city: 'São Paulo',
-      neighborhood: 'Perdizes',
-      street: 'Rua Caiubi',
+    test('Utilizando um CEP válido retornar apenas cidade: 05010000', async () => {
+      const requestUrl = `${global.SERVER_URL}/api/graphql/v1`;
+      const response = await axios.post(requestUrl, {
+        query: `query FetchCep($cep: String!){
+                  cep(cep: $cep) {
+                      city
+                  }
+              }`,
+        variables: {
+          cep: '05010000',
+        },
+      });
+
+      expect(response.data.data.cep).toEqual({
+        city: 'São Paulo'
+      });
+    });
+
+    test('Utilizando um CEP inexistente: 00000000', async () => {
+      const requestUrl = `${global.SERVER_URL}/api/graphql/v1`;
+      const response = await axios.post(requestUrl, {
+        query: `query FetchCep($cep: String!){
+                  cep(cep: $cep) {
+                      cep
+                      state
+                      city
+                      street
+                      neighborhood
+                  }
+              }`,
+        variables: {
+          cep: '00000000',
+        },
+      });
+
+      expect(response.data.data.cep).toEqual(null);
+      expect(response.data.errors[0].message).toEqual('Erro ao consultar CEP');
+      expect(response.data.errors[0].extensions.code).toEqual('service_error');
+    });
+
+    test('Utilizando um CEP inválido: 999999999999999', async () => {
+      const requestUrl = `${global.SERVER_URL}/api/graphql/v1`;
+      const response = await axios.post(requestUrl, {
+        query: `query FetchCep($cep: String!){
+                  cep(cep: $cep) {
+                      cep
+                      state
+                      city
+                      street
+                      neighborhood
+                  }
+              }`,
+        variables: {
+          cep: '999999999999999',
+        },
+      });
+
+      expect(response.data.data.cep).toEqual(null);
+      expect(response.data.errors[0].message).toEqual('CEP inválido');
+      expect(response.data.errors[0].extensions.code).toEqual('validation_error');
     });
   });
 
-  test('Utilizando um CEP inexistente: 00000000', async () => {
-    // O endpoint /cep/v1 está retornando 404 independente
-    // do CEP não existir ou ele não ser válido. Podemos melhorar
-    // esse comportamento fazendo uma diferenciação no Status do
-    // response para quando for um type "validation_error" ou "service_error"
-    // Nesse caso aqui seria um "service_error":
-    // "Todos os serviços de CEP retornaram erro."
-    const requestUrl = `${global.SERVER_URL}/api/graphql/v1`;
-    const response = await axios.post(requestUrl, {
-      query: `query FetchCep($cep: String!){
-                cep(cep: $cep) {
-                    cep
-                    state
-                    city
-                    street
-                    neighborhood
-                }
-            }`,
-      variables: {
-        cep: '00000000',
-      },
+  describe('Banks', () => {
+    test('Utilizando um bank code válido: 260', async () => {
+      const requestUrl = `${global.SERVER_URL}/api/graphql/v1`;
+      const response = await axios.post(requestUrl, {
+        query: `query FetchBank($code: Int!){
+                  bank(code: $code) {
+                      ispb
+                      name
+                      code
+                      fullName
+                  }
+              }`,
+        variables: {
+          code: 260
+        }
+      });
+
+      expect(response.data.data.bank).toEqual({
+        ispb: '18236120',
+        name: 'NU PAGAMENTOS S.A.',
+        code: 260,
+        fullName: 'Nu Pagamentos S.A.',
+      });
     });
 
-    expect(response.data.data.cep).toEqual(null);
-    expect(response.data.errors[0].message).toEqual('Erro ao consultar CEP');
-    expect(response.data.errors[0].extensions.code).toEqual('service_error');
-  });
+    test('Utilizando um bank code válido retornar apenas nome do banco: 260', async () => {
+      const requestUrl = `${global.SERVER_URL}/api/graphql/v1`;
+      const response = await axios.post(requestUrl, {
+        query: `query FetchBank($code: Int!){
+                  bank(code: $code) {
+                      name
+                  }
+              }`,
+        variables: {
+          code: 260
+        }
+      });
 
-  test('Utilizando um CEP inválido: 999999999999999', async () => {
-    // O endpoint /cep/v1 está retornando 404 independente
-    // do CEP não existir ou ele não ser válido. Podemos melhorar
-    // esse comportamento fazendo uma diferenciação no Status do
-    // response para quando for um type "validation_error" ou "service_error"
-    // Nesse caso aqui seria um "validation_error":
-    // "CEP deve conter exatamente 8 caracteres."
-    const requestUrl = `${global.SERVER_URL}/api/graphql/v1`;
-    const response = await axios.post(requestUrl, {
-      query: `query FetchCep($cep: String!){
-                cep(cep: $cep) {
-                    cep
-                    state
-                    city
-                    street
-                    neighborhood
-                }
-            }`,
-      variables: {
-        cep: '999999999999999',
-      },
+      expect(response.data.data.bank).toEqual({
+        name: 'NU PAGAMENTOS S.A.'
+      });
     });
 
-    expect(response.data.data.cep).toEqual(null);
-    expect(response.data.errors[0].message).toEqual('CEP inválido');
-    expect(response.data.errors[0].extensions.code).toEqual('validation_error');
+    test('Listando todos os bancos', async () => {
+      const requestUrl = `${global.SERVER_URL}/api/graphql/v1`;
+      const response = await axios.post(requestUrl, {
+        query: `query{
+                  banks {
+                      ispb
+                      name
+                      code
+                      fullName
+                  }
+              }`
+      });
+
+      expect(response.status).toBe(200);
+      expect(Array.isArray(response.data.data.banks)).toBe(true);
+    });
   });
 });
