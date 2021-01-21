a3b2349dfae207db11198c93957a1cb09a505faf
matheuslcandido <matheus.l.candido@gmail.com>
2021-01-21 16:03:39 -0300

    ddd: change ddd endpoint service
    Change the ddd endpoint to use the
    new ddd service based on a .csv file
    provided by govBr.

--- tests/ddd-v1.test.js
+++ tests/ddd-v1.test.js
@@ -1,26 +1,27 @@
 const axios = require('axios');
 
-const scenariosDdd = {
-  sucess: require('./helpers/scenarios/ddd/success'),
-  inexistent: require('./helpers/scenarios/ddd/inexistent'),
-  incorrect: require('./helpers/scenarios/ddd/incorrect'),
-};
+const { validResponse, invalidResponse } = require('./helpers/ddd');
 
 const requestUrl = `${global.SERVER_URL}/api/ddd/v1`;
 
-describe.skip('api/ddd/v1 (E2E)', () => {
-  test('Utilizando um DDD válido: 12', async () => {
-    const response = await axios.get(`${requestUrl}/12`);
-    expect(response.data).toEqual(scenariosDdd.sucess);
+describe('api/ddd/v1 (E2E)', () => {
+  test('Utilizando um DDD válido: 11', async () => {
+    const response = await axios.get(`${requestUrl}/11`);
+    const { data, status } = response;
+
+    expect(status).toEqual(200);
+    expect(data).toEqual(validResponse);
   });
 
   test('Utilizando um DDD inexistente: 01', async () => {
-    const response = await axios.get(`${requestUrl}/01`);
-    expect(response.data).toEqual(scenariosDdd.inexistent);
-  });
+    try {
+      await axios.get(`${requestUrl}/01`);
+    } catch (error) {
+      const { response } = error;
+      const { data, status } = response;
 
-  test('Utilizando um DDD inválido: 9999', async () => {
-    const response = await axios.get(`${requestUrl}/9999`);
-    expect(response.data).toEqual(scenariosDdd.incorrect);
+      expect(status).toEqual(404);
+      expect(data).toEqual(invalidResponse);
+    }
   });
 });
