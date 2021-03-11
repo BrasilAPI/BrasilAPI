dbec4947ecbbdd2b18a60234171c41af7c6b48fb
unknown <pedrolol73@gmail.com>
2021-03-11 11:12:23 -0300

    kiritokunkun: Removendo DRY

--- tests/ibge-municipios-v1.test.js
+++ tests/ibge-municipios-v1.test.js
@@ -1,59 +1,59 @@
 const axios = require('axios');
 
+const validTestArray = expect.arrayContaining([
+  expect.objectContaining({
+    id: expect.any(Number),
+    nome: expect.any(String),
+    municipio: expect.objectContaining({
+      id: expect.any(Number),
+      nome: expect.any(String),
+      microrregiao: expect.objectContaining({
+        id: expect.any(Number),
+        nome: expect.any(String),
+        mesorregiao: expect.objectContaining({
+          id: expect.any(Number),
+          nome: expect.any(String),
+          UF: expect.objectContaining({
+            id: expect.any(Number),
+            sigla: expect.any(String),
+            nome: expect.any(String),
+            regiao: expect.objectContaining({
+              id: expect.any(Number),
+              sigla: expect.any(String),
+              nome: expect.any(String),
+            }),
+          }),
+        }),
+      }),
+      'regiao-imediata': expect.objectContaining({
+        id: expect.any(Number),
+        nome: expect.any(String),
+        'regiao-intermediaria': expect.objectContaining({
+          id: expect.any(Number),
+          nome: expect.any(String),
+          UF: expect.objectContaining({
+            id: expect.any(Number),
+            sigla: expect.any(String),
+            nome: expect.any(String),
+            regiao: expect.objectContaining({
+              id: expect.any(Number),
+              sigla: expect.any(String),
+              nome: expect.any(String),
+            }),
+          }),
+        }),
+      }),
+    }),
+  }),
+]);
+
 describe('/ibge/municipios/v1 (E2E)', () => {
   test('Utilizando um codigo válido: 22', async () => {
     const requestUrl = `${global.SERVER_URL}/api/ibge/municipios/v1/22`;
     const response = await axios.get(requestUrl);
 
     expect(response.status).toBe(200);
-    expect(response.data).toEqual(
-      expect.arrayContaining([
-        expect.objectContaining({
-          id: expect.any(Number),
-          nome: expect.any(String),
-          municipio: expect.objectContaining({
-            id: expect.any(Number),
-            nome: expect.any(String),
-            microrregiao: expect.objectContaining({
-              id: expect.any(Number),
-              nome: expect.any(String),
-              mesorregiao: expect.objectContaining({
-                id: expect.any(Number),
-                nome: expect.any(String),
-                UF: expect.objectContaining({
-                  id: expect.any(Number),
-                  sigla: expect.any(String),
-                  nome: expect.any(String),
-                  regiao: expect.objectContaining({
-                    id: expect.any(Number),
-                    sigla: expect.any(String),
-                    nome: expect.any(String),
-                  }),
-                }),
-              }),
-            }),
-            'regiao-imediata': expect.objectContaining({
-              id: expect.any(Number),
-              nome: expect.any(String),
-              'regiao-intermediaria': expect.objectContaining({
-                id: expect.any(Number),
-                nome: expect.any(String),
-                UF: expect.objectContaining({
-                  id: expect.any(Number),
-                  sigla: expect.any(String),
-                  nome: expect.any(String),
-                  regiao: expect.objectContaining({
-                    id: expect.any(Number),
-                    sigla: expect.any(String),
-                    nome: expect.any(String),
-                  }),
-                }),
-              }),
-            }),
-          }),
-        }),
-      ])
-    );
+    expect(response.data).toEqual(validTestArray);
   });
 
   test('Utilizando um Codigo inexistente ou inválido: 99', async () => {
@@ -72,54 +72,7 @@ describe('/ibge/municipios/v1 (E2E)', () => {
     const response = await axios.get(requestUrl);
 
     expect(response.status).toBe(200);
-    expect(response.data).toEqual(
-      expect.arrayContaining([
-        expect.objectContaining({
-          id: expect.any(Number),
-          nome: expect.any(String),
-          municipio: expect.objectContaining({
-            id: expect.any(Number),
-            nome: expect.any(String),
-            microrregiao: expect.objectContaining({
-              id: expect.any(Number),
-              nome: expect.any(String),
-              mesorregiao: expect.objectContaining({
-                id: expect.any(Number),
-                nome: expect.any(String),
-                UF: expect.objectContaining({
-                  id: expect.any(Number),
-                  sigla: expect.any(String),
-                  nome: expect.any(String),
-                  regiao: expect.objectContaining({
-                    id: expect.any(Number),
-                    sigla: expect.any(String),
-                    nome: expect.any(String),
-                  }),
-                }),
-              }),
-            }),
-            'regiao-imediata': expect.objectContaining({
-              id: expect.any(Number),
-              nome: expect.any(String),
-              'regiao-intermediaria': expect.objectContaining({
-                id: expect.any(Number),
-                nome: expect.any(String),
-                UF: expect.objectContaining({
-                  id: expect.any(Number),
-                  sigla: expect.any(String),
-                  nome: expect.any(String),
-                  regiao: expect.objectContaining({
-                    id: expect.any(Number),
-                    sigla: expect.any(String),
-                    nome: expect.any(String),
-                  }),
-                }),
-              }),
-            }),
-          }),
-        }),
-      ])
-    );
+    expect(response.data).toEqual(validTestArray);
   });
 
   test('Utilizando uma sigla válida: RS', async () => {
@@ -127,54 +80,7 @@ describe('/ibge/municipios/v1 (E2E)', () => {
     const response = await axios.get(requestUrl);
 
     expect(response.status).toBe(200);
-    expect(response.data).toEqual(
-      expect.arrayContaining([
-        expect.objectContaining({
-          id: expect.any(Number),
-          nome: expect.any(String),
-          municipio: expect.objectContaining({
-            id: expect.any(Number),
-            nome: expect.any(String),
-            microrregiao: expect.objectContaining({
-              id: expect.any(Number),
-              nome: expect.any(String),
-              mesorregiao: expect.objectContaining({
-                id: expect.any(Number),
-                nome: expect.any(String),
-                UF: expect.objectContaining({
-                  id: expect.any(Number),
-                  sigla: expect.any(String),
-                  nome: expect.any(String),
-                  regiao: expect.objectContaining({
-                    id: expect.any(Number),
-                    sigla: expect.any(String),
-                    nome: expect.any(String),
-                  }),
-                }),
-              }),
-            }),
-            'regiao-imediata': expect.objectContaining({
-              id: expect.any(Number),
-              nome: expect.any(String),
-              'regiao-intermediaria': expect.objectContaining({
-                id: expect.any(Number),
-                nome: expect.any(String),
-                UF: expect.objectContaining({
-                  id: expect.any(Number),
-                  sigla: expect.any(String),
-                  nome: expect.any(String),
-                  regiao: expect.objectContaining({
-                    id: expect.any(Number),
-                    sigla: expect.any(String),
-                    nome: expect.any(String),
-                  }),
-                }),
-              }),
-            }),
-          }),
-        }),
-      ])
-    );
+    expect(response.data).toEqual(validTestArray);
   });
 
   test('Utilizando um sigla inexistente ou inválida: AA', async () => {
