82ed87cf0eb4039af6b7ccd27bd3ae96bbfecd3d
unknown <pedrolol73@gmail.com>
2021-03-11 01:59:00 -0300

    kiritokunkun: Consulta de municipios por estado

--- services/ibge.js
+++ services/ibge.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const urlUf = ' https://servicodados.ibge.gov.br/api/v1/localidades/estados';
+const urlUf = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados';
 
 export const getUfs = () => axios.get(urlUf);
 export const getUfByCode = (code) => axios.get(`${urlUf}/${code}`);
+
+export const getDistrictsByUf = (uf) => axios.get(`${urlUf}/${uf}/distritos`);
---
+++ tests/ibge-municipios-v1.test.js
@@ -0,0 +1,195 @@
+const axios = require('axios');
+
+describe('/ibge/municipios/v1 (E2E)', () => {
+  test('Utilizando um codigo válido: 22', async () => {
+    const requestUrl = `${global.SERVER_URL}/api/ibge/municipios/v1/22`;
+    const response = await axios.get(requestUrl);
+
+    expect(response.status).toBe(200);
+    expect(response.data).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          id: expect.any(Number),
+          nome: expect.any(String),
+          municipio: expect.objectContaining({
+            id: expect.any(Number),
+            nome: expect.any(String),
+            microrregiao: expect.objectContaining({
+              id: expect.any(Number),
+              nome: expect.any(String),
+              mesorregiao: expect.objectContaining({
+                id: expect.any(Number),
+                nome: expect.any(String),
+                UF: expect.objectContaining({
+                  id: expect.any(Number),
+                  sigla: expect.any(String),
+                  nome: expect.any(String),
+                  regiao: expect.objectContaining({
+                    id: expect.any(Number),
+                    sigla: expect.any(String),
+                    nome: expect.any(String),
+                  }),
+                }),
+              }),
+            }),
+            'regiao-imediata': expect.objectContaining({
+              id: expect.any(Number),
+              nome: expect.any(String),
+              'regiao-intermediaria': expect.objectContaining({
+                id: expect.any(Number),
+                nome: expect.any(String),
+                UF: expect.objectContaining({
+                  id: expect.any(Number),
+                  sigla: expect.any(String),
+                  nome: expect.any(String),
+                  regiao: expect.objectContaining({
+                    id: expect.any(Number),
+                    sigla: expect.any(String),
+                    nome: expect.any(String),
+                  }),
+                }),
+              }),
+            }),
+          }),
+        }),
+      ])
+    );
+  });
+
+  test('Utilizando um Codigo inexistente ou inválido: 99', async () => {
+    const requestUrl = `${global.SERVER_URL}/api/ibge/municipios/v1/99`;
+
+    try {
+      await axios.get(requestUrl);
+    } catch (error) {
+      const { response } = error;
+      expect(response.status).toBe(404);
+    }
+  });
+
+  test('Utilizando uma sigla válida: SC', async () => {
+    const requestUrl = `${global.SERVER_URL}/api/ibge/municipios/v1/SC`;
+    const response = await axios.get(requestUrl);
+
+    expect(response.status).toBe(200);
+    expect(response.data).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          id: expect.any(Number),
+          nome: expect.any(String),
+          municipio: expect.objectContaining({
+            id: expect.any(Number),
+            nome: expect.any(String),
+            microrregiao: expect.objectContaining({
+              id: expect.any(Number),
+              nome: expect.any(String),
+              mesorregiao: expect.objectContaining({
+                id: expect.any(Number),
+                nome: expect.any(String),
+                UF: expect.objectContaining({
+                  id: expect.any(Number),
+                  sigla: expect.any(String),
+                  nome: expect.any(String),
+                  regiao: expect.objectContaining({
+                    id: expect.any(Number),
+                    sigla: expect.any(String),
+                    nome: expect.any(String),
+                  }),
+                }),
+              }),
+            }),
+            'regiao-imediata': expect.objectContaining({
+              id: expect.any(Number),
+              nome: expect.any(String),
+              'regiao-intermediaria': expect.objectContaining({
+                id: expect.any(Number),
+                nome: expect.any(String),
+                UF: expect.objectContaining({
+                  id: expect.any(Number),
+                  sigla: expect.any(String),
+                  nome: expect.any(String),
+                  regiao: expect.objectContaining({
+                    id: expect.any(Number),
+                    sigla: expect.any(String),
+                    nome: expect.any(String),
+                  }),
+                }),
+              }),
+            }),
+          }),
+        }),
+      ])
+    );
+  });
+
+  test('Utilizando uma sigla válida: RS', async () => {
+    const requestUrl = `${global.SERVER_URL}/api/ibge/municipios/v1/RS`;
+    const response = await axios.get(requestUrl);
+
+    expect(response.status).toBe(200);
+    expect(response.data).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          id: expect.any(Number),
+          nome: expect.any(String),
+          municipio: expect.objectContaining({
+            id: expect.any(Number),
+            nome: expect.any(String),
+            microrregiao: expect.objectContaining({
+              id: expect.any(Number),
+              nome: expect.any(String),
+              mesorregiao: expect.objectContaining({
+                id: expect.any(Number),
+                nome: expect.any(String),
+                UF: expect.objectContaining({
+                  id: expect.any(Number),
+                  sigla: expect.any(String),
+                  nome: expect.any(String),
+                  regiao: expect.objectContaining({
+                    id: expect.any(Number),
+                    sigla: expect.any(String),
+                    nome: expect.any(String),
+                  }),
+                }),
+              }),
+            }),
+            'regiao-imediata': expect.objectContaining({
+              id: expect.any(Number),
+              nome: expect.any(String),
+              'regiao-intermediaria': expect.objectContaining({
+                id: expect.any(Number),
+                nome: expect.any(String),
+                UF: expect.objectContaining({
+                  id: expect.any(Number),
+                  sigla: expect.any(String),
+                  nome: expect.any(String),
+                  regiao: expect.objectContaining({
+                    id: expect.any(Number),
+                    sigla: expect.any(String),
+                    nome: expect.any(String),
+                  }),
+                }),
+              }),
+            }),
+          }),
+        }),
+      ])
+    );
+  });
+
+  test('Utilizando um sigla inexistente ou inválida: AA', async () => {
+    const requestUrl = `${global.SERVER_URL}/api/ibge/municipios/v1/AA`;
+
+    try {
+      await axios.get(requestUrl);
+    } catch (error) {
+      const { response } = error;
+      expect(response.status).toBe(404);
+      expect(response.data).toMatchObject({
+        name: 'NotFoundError',
+        message: 'UF não encontrado.',
+        type: 'not_found',
+      });
+    }
+  });
+});
