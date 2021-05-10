5dd67ea386249978ee827f6f3ccae330d2572ab4
Lorhan Sohaky <lorhansohaky@gmail.com>
2021-05-10 01:38:50 -0300

    test(fipe): add test for fipe price

--- tests/fipe-v1.test.js
+++ tests/fipe-v1.test.js
@@ -239,3 +239,238 @@ describe('/fipe/marcas/v1/caminhoes (E2E)', () => {
     }
   });
 });
+
+describe('/fipe/preco/v1 (E2E)', () => {
+  test('Utilizando um veículo válido sem informar nenhuma tabela de referência', async () => {
+    expect.assertions(2);
+    const fipeCode = '015088-6';
+    const requestUrl = `${global.SERVER_URL}/api/fipe/preco/v1/${fipeCode}`;
+    const response = await axios.get(requestUrl);
+
+    expect(response.status).toBe(200);
+    expect(response.data).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          valor: expect.any(String),
+          marca: expect.any(String),
+          modelo: expect.any(String),
+          anoModelo: expect.any(Number),
+          combustivel: expect.any(String),
+          codigoFipe: expect.any(String),
+          mesReferencia: expect.any(String),
+          tipoVeiculo: expect.any(Number),
+          siglaCombustivel: expect.any(String),
+          dataConsulta: expect.any(String),
+        }),
+      ])
+    );
+  });
+
+  test('Utilizando um veículo válido com a tabela de referência 267', async () => {
+    expect.assertions(2);
+    const fipeCode = '015088-6';
+    const requestUrl = `${global.SERVER_URL}/api/fipe/preco/v1/${fipeCode}`;
+    const referenceTable = 267;
+
+    const response = await axios.get(requestUrl, {
+      params: {
+        tabela_referencia: referenceTable,
+      },
+    });
+
+    expect(response.status).toBe(200);
+    expect(response.data).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          valor: expect.any(String),
+          marca: expect.any(String),
+          modelo: expect.any(String),
+          anoModelo: expect.any(Number),
+          combustivel: expect.any(String),
+          codigoFipe: expect.any(String),
+          mesReferencia: expect.any(String),
+          tipoVeiculo: expect.any(Number),
+          siglaCombustivel: expect.any(String),
+          dataConsulta: expect.any(String),
+        }),
+      ])
+    );
+  });
+
+  test('Utilizando um carro válido com a tabela de referência 270', async () => {
+    expect.assertions(2);
+    const fipeCode = '004113-0';
+    const requestUrl = `${global.SERVER_URL}/api/fipe/preco/v1/${fipeCode}`;
+    const referenceTable = 270;
+
+    const response = await axios.get(requestUrl, {
+      params: {
+        tabela_referencia: referenceTable,
+      },
+    });
+
+    expect(response.status).toBe(200);
+    expect(response.data).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          valor: expect.any(String),
+          marca: expect.any(String),
+          modelo: expect.any(String),
+          anoModelo: expect.any(Number),
+          combustivel: expect.any(String),
+          codigoFipe: expect.any(String),
+          mesReferencia: expect.any(String),
+          tipoVeiculo: expect.any(Number),
+          siglaCombustivel: expect.any(String),
+          dataConsulta: expect.any(String),
+        }),
+      ])
+    );
+  });
+
+  test('Utilizando uma moto válida com a tabela de referência 270', async () => {
+    expect.assertions(2);
+    const fipeCode = '811165-0';
+    const requestUrl = `${global.SERVER_URL}/api/fipe/preco/v1/${fipeCode}`;
+    const referenceTable = 270;
+
+    const response = await axios.get(requestUrl, {
+      params: {
+        tabela_referencia: referenceTable,
+      },
+    });
+
+    expect(response.status).toBe(200);
+    expect(response.data).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          valor: expect.any(String),
+          marca: expect.any(String),
+          modelo: expect.any(String),
+          anoModelo: expect.any(Number),
+          combustivel: expect.any(String),
+          codigoFipe: expect.any(String),
+          mesReferencia: expect.any(String),
+          tipoVeiculo: expect.any(Number),
+          siglaCombustivel: expect.any(String),
+          dataConsulta: expect.any(String),
+        }),
+      ])
+    );
+  });
+
+  test('Utilizando um caminhão válido com a tabela de referência 270', async () => {
+    expect.assertions(2);
+    const fipeCode = '516094-4';
+    const requestUrl = `${global.SERVER_URL}/api/fipe/preco/v1/${fipeCode}`;
+    const referenceTable = 270;
+
+    const response = await axios.get(requestUrl, {
+      params: {
+        tabela_referencia: referenceTable,
+      },
+    });
+
+    expect(response.status).toBe(200);
+    expect(response.data).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          valor: expect.any(String),
+          marca: expect.any(String),
+          modelo: expect.any(String),
+          anoModelo: expect.any(Number),
+          combustivel: expect.any(String),
+          codigoFipe: expect.any(String),
+          mesReferencia: expect.any(String),
+          tipoVeiculo: expect.any(Number),
+          siglaCombustivel: expect.any(String),
+          dataConsulta: expect.any(String),
+        }),
+      ])
+    );
+  });
+
+  test('Utilizando um veículo válido com a tabela de referência inválida', async () => {
+    expect.assertions(2);
+    const fipeCode = '015088-6';
+    const requestUrl = `${global.SERVER_URL}/api/fipe/preco/v1/${fipeCode}`;
+    const referenceTable = 'AAAA';
+    try {
+      await axios.get(requestUrl, {
+        params: { tabela_referencia: referenceTable },
+      });
+    } catch (error) {
+      const { response } = error;
+
+      expect(response.status).toBe(400);
+      expect(response.data).toEqual({
+        name: 'BadRequestError',
+        message: 'Tabela de referência inválida',
+        type: 'bad_request',
+      });
+    }
+  });
+
+  test('Utilizando um veículo inválido sem informar nenhuma tabela de referência', async () => {
+    expect.assertions(2);
+    const fipeCode = 'AAAAAA-6';
+    const requestUrl = `${global.SERVER_URL}/api/fipe/preco/v1/${fipeCode}`;
+
+    try {
+      await axios.get(requestUrl);
+    } catch (error) {
+      const { response } = error;
+
+      expect(response.status).toBe(400);
+      expect(response.data).toEqual({
+        name: 'BadRequestError',
+        message: 'Código fipe inválido',
+        type: 'bad_request',
+      });
+    }
+  });
+
+  test('Utilizando um veículo inválido com a tabela de referência 267', async () => {
+    expect.assertions(2);
+    const fipeCode = 'AAAAAA-6';
+    const requestUrl = `${global.SERVER_URL}/api/fipe/preco/v1/${fipeCode}`;
+    const referenceTable = 267;
+    try {
+      await axios.get(requestUrl, {
+        params: {
+          tabela_referencia: referenceTable,
+        },
+      });
+    } catch (error) {
+      const { response } = error;
+
+      expect(response.status).toBe(400);
+      expect(response.data).toEqual({
+        name: 'BadRequestError',
+        message: 'Código fipe inválido',
+        type: 'bad_request',
+      });
+    }
+  });
+
+  test('Utilizando um veículo inválido com a tabela de referência inválida', async () => {
+    expect.assertions(2);
+    const fipeCode = 'AAAAAA-6';
+    const requestUrl = `${global.SERVER_URL}/api/fipe/preco/v1/${fipeCode}`;
+    const referenceTable = 'AAAA';
+    try {
+      await axios.get(requestUrl, {
+        params: { tabela_referencia: referenceTable },
+      });
+    } catch (error) {
+      const { response } = error;
+
+      expect(response.status).toBe(400);
+      expect(response.data).toEqual({
+        name: 'BadRequestError',
+        message: 'Tabela de referência inválida',
+        type: 'bad_request',
+      });
+    }
+  });
+});
