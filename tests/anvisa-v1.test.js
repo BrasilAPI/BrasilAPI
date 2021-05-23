d477ba104254d767acf4e4e7ef9c8d4439651a5a
leonardovee@hotmail.com <leonardo@hotmail.com>
2021-05-23 01:22:12 -0300

    test: create anvisa endpoint

---
+++ tests/anvisa-v1.test.js
@@ -0,0 +1,43 @@
+const axios = require('axios');
+
+const requestUrl = `${global.SERVER_URL}/api/anvisa/v1`;
+
+describe('api/anvisa/v1 (E2E)', () => {
+  test('Utilizando um ANVISA válido existente: 10064010018', async () => {
+    const response = await axios.get(`${requestUrl}/10064010018`);
+    const { data, status } = response;
+
+    expect(status).toEqual(200);
+    expect(data.produto).toEqual(
+      'KAVO KEY LASER 1242/1243  - TRATAMENTO DE CARIES ETERAPIAS EM TECIDOS DUROS'
+    );
+  });
+
+  test('Utilizando um ANVISA válido inexistente: 0000000000', async () => {
+    try {
+      await axios.get(`${requestUrl}/0000000000`);
+    } catch (error) {
+      const { response } = error;
+      const { data, status } = response;
+
+      expect(status).toEqual(404);
+      expect(data).toEqual({
+        message: 'Protocolo do ANVISA 0000000000 não encontrado.',
+      });
+    }
+  });
+
+  test('Utilizando um ANVISA inválido: 123', async () => {
+    try {
+      await axios.get(`${requestUrl}/123`);
+    } catch (error) {
+      const { response } = error;
+      const { data, status } = response;
+
+      expect(status).toEqual(400);
+      expect(data).toEqual({
+        message: 'Protocolo do ANVISA 0000000000 não encontrado.',
+      });
+    }
+  });
+});
