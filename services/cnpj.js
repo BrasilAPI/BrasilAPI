d587566099929093b324f9a19dac7891e638e57e
Eduardo Cuducos <cuducos@users.noreply.github.com>
2021-05-27 10:11:58 -0400

    Updates Minha Receita API

--- services/cnpj.js
+++ services/cnpj.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
 export const getCnpjData = async (cnpj) => {
-  const url = 'https://minhareceita.org/';
-  const response = await axios({ url, method: 'post', data: `cnpj=${cnpj}` });
+  const response = await axios.get(`https://minhareceita.org/${cnpj}`);
   return response;
 };
