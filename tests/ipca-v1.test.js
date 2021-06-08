6aeec38636b58fd93c6fdf3b72583dee2aa35377
Enzo Ferrari <ferrarienzobf@gmail.com>
2021-06-08 10:02:24 -0300

    test(ipca-v1.test.js): test all IPCA fetching routes
    All the routes are tested, with all possible limiting parameters (last n values, or period)

---
+++ tests/ipca-v1.test.js
@@ -0,0 +1,51 @@
+const axios = require('axios');
+
+describe('ipca v1 (E2E)', () => {
+  describe('GET indicadores/ipca/v1', () => {
+    test('Sem parâmetro de limite', async () => {
+      const requestUrl = `${global.SERVER_URL}/api/indicadores/ipca/v1/`;
+      const response = await axios.get(requestUrl);
+
+      expect(response.status).toBe(200);
+      expect(response.data).toEqual(
+        expect.arrayContaining([{ data: '01/02/1991', valor: '19.62' }])
+      );
+    });
+
+    test('Com de limite', async () => {
+      const requestUrl = `${global.SERVER_URL}/api/indicadores/ipca/v1?last=2`;
+      const response = await axios.get(requestUrl);
+
+      expect(response.status).toBe(200);
+      expect(response.data.length).toBe(2);
+    });
+  });
+
+  describe('GET /ipca/v1/periodo', () => {
+    test('Com intervalo válido', async () => {
+      const requestUrl = `${global.SERVER_URL}/api/indicadores/ipca/v1/periodo?startDate="01/01/2020"&endDate="01/05/2020"`;
+      const response = await axios.get(requestUrl);
+
+      expect(response.status).toBe(200);
+      expect(response.data).toEqual(
+        expect.arrayContaining([
+          { data: '01/01/2020', valor: '0.17' },
+          { data: '01/02/2020', valor: '0.35' },
+          { data: '01/03/2020', valor: '0.04' },
+          { data: '01/04/2020', valor: '0.01' },
+          { data: '01/05/2020', valor: '-0.19' },
+        ])
+      );
+    });
+
+    test('Com intervalo inválido', async () => {
+      const requestUrl = `${global.SERVER_URL}/api/indicadores/ipca/v1/periodo?startDate="01/05/2020"&endDate="01/01/2020"`;
+      const response = await axios.get(requestUrl);
+
+      expect(response.status).toBe(200);
+      expect(response.data).toEqual(
+        expect.arrayContaining([{ data: '01/05/2020', valor: '-0.19' }])
+      );
+    });
+  });
+});
