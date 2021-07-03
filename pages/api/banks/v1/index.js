daa43ff935d284415d05df4f4685e23336710ee9
Enzo Ferrari <ferrarienzobf@gmail.com>
2021-07-02 23:42:43 -0300

    style(services/banco-central/banks): rename bank.js to index.js
    Prevent import redundancy

--- pages/api/banks/v1/index.js
+++ pages/api/banks/v1/index.js
@@ -1,5 +1,5 @@
 import app from '@/app';
-import { getBanksData } from '@/services/banco-central/banks/banks';
+import { getBanksData } from '@/services/banco-central/banks';
 
 const action = async (request, response) => {
   const allBanksData = await getBanksData();
