936decf73929727a7f42404185a156065614d302
Lorhan Sohaky <lorhansohaky@gmail.com>
2021-09-27 11:59:49 -0300

    chore: fix critical request

--- pages/_document.js
+++ pages/_document.js
@@ -23,19 +23,19 @@ export default class MyDocument extends Document {
             href="/favicon-16x16.png"
           />
 
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Inter&family=Lexend:wght@400;600;700&display=swap"
-            rel="stylesheet"
-            property="stylesheet"
-          />
-
           <link rel="shortcut icon" href="/favicon.png" type="image/png" />
         </Head>
 
         <body>
           <Main />
           <NextScript />
+
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter&family=Lexend:wght@400;600;700&display=swap"
+            rel="stylesheet"
+            property="stylesheet"
+          />
         </body>
       </Html>
     );
