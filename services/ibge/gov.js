3cf1ed554bc1ab47a4bef36a13f1260dda5743e9
Rui Guilherme <guilhermecunha.rui@gmail.com>
2021-11-05 13:57:37 -0300

    gov para async

--- services/ibge/gov.js
+++ services/ibge/gov.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 const URL_UF = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados';
 
-export const getUfs = () => axios.get(URL_UF);
-export const getUfByCode = (code) => axios.get(`${URL_UF}/${code}`);
+export const getUfs = async () => await axios.get(URL_UF);
+export const getUfByCode = async (code) => await axios.get(`${URL_UF}/${code}`);
 
 export const getDistrictsByUf = async (uf) => {
   const { data } = await axios.get(`${URL_UF}/${uf}/distritos`);
