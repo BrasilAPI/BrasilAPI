0e2a75f450baa1b16eeaf9e219e2b61e6c62055b
Samuel Cunha <samukasaww@live.com>
2022-11-14 21:43:58 -0300

    DDD Adicionando novas APIs
    Co-authored-by: Hermes Agent <noreply@nousresearch.com>
    Ai-tool: hermes
    Ai-model: deepseek-v4-flash
    Ai-os: wsl

--- tests/ddd-v1.test.js
+++ tests/ddd-v1.test.js
@@ -1,14 +1,21 @@
 import axios from 'axios';
-import { beforeAll, describe, expect, test } from 'vitest';
 
-import { testCorsForRoute } from './helpers/cors';
-import { validResponse, invalidResponseInvalid } from './helpers/ddd';
+import {
+  validResponseV1,
+  validResponseAllDDDV1,
+  invalidResponse,
+} from './helpers/ddd';
+
+const requestUrl = `${global.SERVER_URL}/api/ddd/v1`;
 
 describe('api/ddd/v1 (E2E)', () => {
-  let requestUrl = '';
+  test('Obtendo todos os DDD válidos', async () => {
+    const response = await axios.get(`${requestUrl}`);
+    const { data, status } = response;
 
-  beforeAll(async () => {
-    requestUrl = `${global.SERVER_URL}/api/ddd/v1`;
+    expect(status).toEqual(200);
+    const dddSP = data.filter((ddd) => ddd.state === 'SP');
+    expect(dddSP).toEqual(validResponseAllDDDV1);
   });
 
   test('Utilizando um DDD válido: 11', async () => {
@@ -16,39 +23,7 @@ describe('api/ddd/v1 (E2E)', () => {
     const { data, status } = response;
 
     expect(status).toEqual(200);
-    expect(data).toEqual(validResponse);
-  });
-
-  test('Utilizando um DDD válido: 011', async () => {
-    const response = await axios.get(`${requestUrl}/011`);
-    const { data, status } = response;
-
-    expect(status).toEqual(200);
-    expect(data).toEqual(validResponse);
-  });
-
-  test('Utilizando um DDD inválido: 111', async () => {
-    try {
-      await axios.get(`${requestUrl}/111`);
-    } catch (error) {
-      const { response } = error;
-      const { data, status } = response;
-
-      expect(status).toEqual(400);
-      expect(data).toEqual(invalidResponseInvalid);
-    }
-  });
-
-  test('Utilizando um DDD inválido: 1', async () => {
-    try {
-      await axios.get(`${requestUrl}/1`);
-    } catch (error) {
-      const { response } = error;
-      const { data, status } = response;
-
-      expect(status).toEqual(400);
-      expect(data).toEqual(invalidResponseInvalid);
-    }
+    expect(data).toEqual(validResponseV1);
   });
 
   test('Utilizando um DDD inexistente: 01', async () => {
@@ -58,10 +33,8 @@ describe('api/ddd/v1 (E2E)', () => {
       const { response } = error;
       const { data, status } = response;
 
-      expect(status).toEqual(400);
-      expect(data).toEqual(invalidResponseInvalid);
+      expect(status).toEqual(404);
+      expect(data).toEqual(invalidResponse);
     }
   });
 });
-
-testCorsForRoute('/api/ddd/v1/11');
---
+++ tests/ddd-v2.test.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+import { validResponseV2, invalidResponse } from './helpers/ddd';
+
+const requestUrl = `${global.SERVER_URL}/api/ddd/v2`;
+
+describe('api/ddd/v2 (E2E)', () => {
+  test('Utilizando um DDD válido: 11', async () => {
+    const response = await axios.get(`${requestUrl}/11`);
+    const { data, status } = response;
+
+    expect(status).toEqual(200);
+    expect(data).toEqual(validResponseV2);
+  });
+
+  test('Utilizando um DDD inexistente: 01', async () => {
+    try {
+      await axios.get(`${requestUrl}/01`);
+    } catch (error) {
+      const { response } = error;
+      const { data, status } = response;
+
+      expect(status).toEqual(404);
+      expect(data).toEqual(invalidResponse);
+    }
+  });
+});
