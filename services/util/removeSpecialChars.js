c4987721186b9d4e0efe81b23c1d2b4263f43776
Rodrigo Teixeira Andreotti <ro.andriotti@gmail.com>
2022-12-14 09:54:09 -0300

    Refactor removeSpecialChars

--- services/util/removeSpecialChars.js
+++ services/util/removeSpecialChars.js
@@ -1,26 +1,5 @@
 export default function removeSpecialChars(text) {
-  const hexaChars = {
-    a: /[\xE0-\xE6]/g,
-    A: /[\xC0-\xC6]/g,
-    e: /[\xE8-\xEB]/g,
-    E: /[\xC8-\xCB]/g,
-    i: /[\xEC-\xEF]/g,
-    I: /[\xCC-\xCF]/g,
-    o: /[\xF2-\xF6]/g,
-    O: /[\xD2-\xD6]/g,
-    u: /[\xF9-\xFC]/g,
-    U: /[\xD9-\xDC]/g,
-    c: /\xE7/g,
-    C: /\xC7/g,
-    n: /\xF1/g,
-    N: /\xD1/g,
-  };
-
-  let newText = text;
-  Object.keys(hexaChars).forEach((letter) => {
-    const regex = hexaChars[letter];
-    newText = newText.replace(regex, letter);
-  });
+  const newText = text.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
 
   return newText.toLowerCase().replace(/[^\w ]+/g, '');
 }
