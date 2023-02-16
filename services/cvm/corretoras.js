b119783c1740b20d29deafc0c73766adfd990ac7
Rodrigo Souza <rodrigo.bressan09@gmail.com>
2023-02-16 20:22:07 -0300

    adicionado todos os campos para a corretora

--- services/cvm/corretoras.js
+++ services/cvm/corretoras.js
@@ -21,43 +21,71 @@ function getIntermediatorsFromZip(data) {
   if (!intermediatorsEntry) {
     throw new Error('File not found');
   }
+
   const intermediatorFile = zip.readAsText(intermediatorsEntry, 'latin1');
   const lines = intermediatorFile.split(LINE_BREAK);
   lines.shift();
 
-  const mappedLines = lines
-    .map((line) => line.split(';')) // Gera um array por coluna
-    .filter(([type, cnpj]) => type && cnpj) // Filtra apenas linhas válidas
-    .map(
-      ([
-        type, // Tipo de participante
-        cnpj, // Cadastro Nacional de Pessoas Jurídicas (CNPJ)
-        socialName, // Denominação Social
-        commercialName, // Denominação Comercial // Data de Registro // Data de Cancelamento // Motivo do Cancelamento
-        ,
-        ,
-        ,
-        status, // Situação
-      ]) => {
-        return {
+  try {
+    const mappedLines = lines
+      .map((line) => line.split(';')) // Gera um array por coluna
+      .filter(([type, cnpj]) => type === 'CORRETORAS' && cnpj)
+      .map(
+        ([
+          type, // Tipo de participante
+          cnpj, // Cadastro Nacional de Pessoas Jurídicas (CNPJ)
+          socialName, // Denominação Social
+          commercialName, // Denominação Comercial
+          registerDate, // Data de Registro // Data de Cancelamento // Motivo do Cancelamento
+          ,
+          ,
+          status, // Situação
+          statusInit, // Data Inicial do Status
+          cvmCode, // Código da CVM // Setor de Atividade // Controle Acionário
+          ,
+          ,
+          equityValue, // Patrimônio Líquido
+          equityValueDate, // Data de Divulgação do Patrimônio Líquido // Tipo de Endereço
+          ,
+          address, // Logradouro
+          obs, // Complemento
+          neighborhood, // Bairro
+          city, // Cidade
+          state, // Estado
+          country, // País
+          cep, // CEP // DDD Telefone
+          ,
+          tel, // Telefone // DDD Fax // Fax
+          ,
+          ,
+          email, // Email
+        ]) => ({
           cnpj: cnpj.replace(/\D/gim, ''),
           type,
           socialName: socialName ? socialName.trim() : '',
           commercialName: commercialName ? commercialName.trim() : '',
           status,
-        };
-      }
-    )
-    .filter(
-      (item) =>
-        item.status === 'EM FUNCIONAMENTO NORMAL' && item.type === 'CORRETORAS'
-    )
-    .map((corretora) => ({
-      cnpj: corretora.cnpj,
-      nome_social: corretora.socialName,
-      nome_comercial: corretora.commercialName,
-    }));
-  return mappedLines;
+          email,
+          telefone: tel,
+          cep,
+          pais: country,
+          uf: state,
+          municipio: city,
+          bairro: neighborhood,
+          complemento: obs,
+          logradouro: address,
+          data_patrimonio_liquido: equityValue,
+          valor_patrimonio_liquido: equityValueDate,
+          codigo_cvm: cvmCode,
+          data_inicio_situacao: statusInit,
+          data_registro: registerDate,
+        })
+      )
+      .filter((item) => item.type === 'CORRETORAS');
+    return mappedLines;
+  } catch (err) {
+    throw new Error(err);
+  }
 }
 
 export const getExchangesData = async () => {
--- tests/corretoras-v1.test.js
+++ tests/corretoras-v1.test.js
@@ -2,9 +2,23 @@ const axios = require('axios');
 
 const validTestTableArray = expect.arrayContaining([
   expect.objectContaining({
+    bairro: expect.any(String),
+    cep: expect.any(String),
     cnpj: expect.any(String),
+    codigo_cvm: expect.any(String),
+    complemento: expect.any(String),
+    data_inicio_situacao: expect.any(String),
+    data_patrimonio_liquido: expect.any(String),
+    data_registro: expect.any(String),
+    email: expect.any(String),
+    logradouro: expect.any(String),
+    municipio: expect.any(String),
     nome_social: expect.any(String),
     nome_comercial: expect.any(String),
+    pais: expect.any(String),
+    telefone: expect.any(String),
+    uf: expect.any(String),
+    valor_patrimonio_liquido: expect.any(String),
   }),
 ]);
 
@@ -16,9 +30,23 @@ describe('corretoras v1 (E2E)', () => {
 
       expect(response.status).toBe(200);
       expect(response.data).toEqual({
+        bairro: 'LEBLON',
+        cep: '22440032',
         cnpj: '02332886000104',
+        codigo_cvm: '3247',
+        complemento: 'SALA 201',
+        data_inicio_situacao: '1998-02-10',
+        data_patrimonio_liquido: '5514593491.29',
+        data_registro: '1997-12-05',
+        email: 'juridico.regulatorio@xpi.com.br',
+        logradouro: 'AVENIDA ATAULFO DE PAIVA 153',
+        municipio: 'RIO DE JANEIRO',
         nome_social: 'XP INVESTIMENTOS CCTVM S.A.',
         nome_comercial: 'XP INVESTIMENTOS',
+        pais: '',
+        telefone: '30272237',
+        uf: 'RJ',
+        valor_patrimonio_liquido: '2021-12-31',
       });
     });
 
