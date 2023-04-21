41e114eabd907352c4608edfc418a73f0fb5e574
Gabriell Huver <gabriell.is.huver@gmail.com>
2023-04-21 11:45:42 -0300

    Update pages/api/senado/v1/orcamento/receita.js
    Co-authored-by: Lorhan Sohaky <16273730+LorhanSohaky@users.noreply.github.com>

--- pages/api/senado/v1/orcamento/receita.js
+++ pages/api/senado/v1/orcamento/receita.js
@@ -5,7 +5,7 @@ import { getDadosReceita } from '@/services/dados-abertos-br/senado-orcamento';
 async function getDadosReceitas(request, response) {
   try {
     const result = await getDadosReceita();
-    return response.status(result.status).json(result.data);
+    return response.status(200).json(result.data);
   } catch (error) {
     throw new InternalError({ message: error.response.data.message });
   }
