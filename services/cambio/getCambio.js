86c5a1eeab816de2475f136d184f5522d096ac17
Vinicius Hora <vinidahora08@hotmail.com>
2023-06-21 00:53:31 -0300

    Arrumando testes com erros, os retornos esperados de alguns testes mudaram

--- services/cambio/getCambio.js
+++ services/cambio/getCambio.js
@@ -1,8 +1,8 @@
-import axios from 'axios';
+// const axios = require('axios');
 
-export const getCambio = async (data) => {
-  const resp = await axios.get(
-    `https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarDia(dataCotacao=@dataCotacao)?@dataCotacao='${data}'&$top=100&$format=json`
-  );
-  return resp;
-};
+// export const getCambio = async (data) => {
+//   const resp = await axios.get(
+//     `https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarDia(dataCotacao=@dataCotacao)?@dataCotacao='${data}'&$top=100&$format=json`
+//   );
+//   return resp;
+// };
