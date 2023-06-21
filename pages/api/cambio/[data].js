aefd83532e7b1d3f6a8a58235a9bcc84e88556b6
Vinicius Hora <vinidahora08@hotmail.com>
2023-06-21 00:16:33 -0300

    cambio: Adicionando cambio de dólar para a data desejada

---
+++ pages/api/cambio/[data].js
@@ -0,0 +1,39 @@
+import app from '@/app';
+import BadRequestError from '@/errors/BadRequestError';
+import { getCambio } from '@/services/cambio/getCambio';
+
+const action = async (request, response) => {
+  const { data } = request.query;
+
+  try {
+    const hoje = new Date();
+    const inicio = new Date(1984, 11, 28);
+    const newData = new Date(
+      data.substring(6, 10),
+      parseInt(data.substring(3, 5), 10) - 1,
+      data.substring(0, 2)
+    );
+
+    if (newData > hoje || newData < inicio) {
+      return response.status(400).json({ message: 'Data inválida' });
+    }
+  } catch (error) {
+    throw new BadRequestError({ message: 'Requisição inválida' });
+  }
+
+  try {
+    const consultaData = `${data.substring(3, 5)}-${data.substring(
+      0,
+      2
+    )}-${data.substring(6, 10)}`;
+
+    const resp = await getCambio(consultaData);
+    return response
+      .status(200)
+      .json({ cotacao: resp.data.value[0].cotacaoCompra });
+  } catch (error) {
+    throw new BadRequestError({ message: 'Problema ao realizar a requisição' });
+  }
+};
+
+export default app(21600).get(action);
---
+++ services/cambio/getCambio.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+export const getCambio = async (data) => {
+  const resp = await axios.get(
+    `https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarDia(dataCotacao=@dataCotacao)?@dataCotacao='${data}'&$top=100&$format=json`
+  );
+  return resp;
+};
