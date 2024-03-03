0f4621e631852e5a73bff09db2921207d5c02610
Enieber Cunha <eniebercunha@gmail.com>
2024-03-03 02:49:59 -0300

    test: add to params weather location
    - add test iinvalid latitude
    - add test invalid longitude
    - add test invalid city

--- tests/cpetc/previsao-v1.test.js
+++ tests/cpetc/previsao-v1.test.js
@@ -88,7 +88,7 @@ describe('weather prediction v1 (E2E)', () => {
     });
   });
   describe('Route WITH lat long', () => {
-    test('GET /api/cptec/v1/clima/previsao/semana/:lat/:long', async () => {
+    test('GET prevision of campina - sp', async () => {
       const requestUrl = `${global.SERVER_URL}/api/cptec/v1/clima/previsao/semana/-22.90/-47.06`;
       const response = await axios.get(requestUrl);
 
@@ -103,6 +103,67 @@ describe('weather prediction v1 (E2E)', () => {
         cidade: 'Campinas',
         estado: 'SP',
       });
+    });
+    // TODO: verify response to positive lat/long
+    /*test('GET prevision of campina - sp with positive lat/long', async () => {
+      const requestUrl = `${global.SERVER_URL}/api/cptec/v1/clima/previsao/semana/22.90/47.06`;
+      const response = await axios.get(requestUrl);
+
+      expect(response.status).toBe(200);
+      expect(Array.isArray(response.data)).toBe(false);
+
+      expect(Array.isArray(response.data.clima)).toBe(true);
+      expect(response.data.clima.length).toBeGreaterThan(2);
+      expect(response.data.clima.length).toBeLessThanOrEqual(7);
+
+      expect(response.data).toMatchObject({
+        cidade: 'Campinas',
+        estado: 'SP',
+      });
+    });*/
+    test('GET prevision of location not in Brazil', async () => {
+      const requestUrl = `${global.SERVER_URL}/api/cptec/v1/clima/previsao/semana/1/1`;
+      try {
+        await axios.get(requestUrl);
+      } catch (error) {
+        const { response } = error;
+
+        expect(response.status).toBe(500);
+
+        expect(response.data).toMatchObject({
+          name: 'CITY_WEATHER_PREDICTIONS_ERROR',
+          type: 'weather_error',
+        });
+      }
+    });
+
+    test('GET with invalid long', async () => {
+      const requestUrl = `${global.SERVER_URL}/api/cptec/v1/clima/previsao/semana/-22.90/abc`;
+      try {
+        await axios.get(requestUrl);
+      } catch (error) {
+        const { response } = error;
+
+        expect(response.status).toBe(400);
+        expect(response.data).toMatchObject({
+          name: 'INVALID_POSITION_LONG',
+          type: 'request_error',
+        });
+      }
+    });
+    test('GET with invalid lat', async () => {
+      const requestUrl = `${global.SERVER_URL}/api/cptec/v1/clima/previsao/semana/abc/-47.06`;
+      try {
+        await axios.get(requestUrl);
+      } catch (error) {
+        const { response } = error;
+
+        expect(response.status).toBe(400);
+        expect(response.data).toMatchObject({
+          name: 'INVALID_POSITION_LAT',
+          type: 'request_error',
+        });
+      }
     });
   });
 });
