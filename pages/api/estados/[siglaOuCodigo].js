8ca082d530137612c57c1a15181794cc19c82eda
Louie <cloud9.alice@gmail.com>
2025-05-09 08:20:05 -0300

    update

--- pages/api/estados/[siglaOuCodigo].js
+++ pages/api/estados/[siglaOuCodigo].js
@@ -6,7 +6,6 @@ import axios from 'axios';
 
 export default async function handler(req, res) {
   const { siglaOuCodigo } = req.query;
-
   try {
     if (!siglaOuCodigo || !CODIGOS_ESTADOS[siglaOuCodigo.toUpperCase()]) {
       throw new NotFoundError({
@@ -14,26 +13,19 @@ export default async function handler(req, res) {
         name: 'EstadoNotFoundException',
       });
     }
-
     const response = await getUfByCode(siglaOuCodigo.toUpperCase());
-
     if (response.status !== 200) {
       throw new Error('Erro na requisição à API do IBGE');
     }
-
     const estado = response.data;
     const idEstado = estado.id;
-
     const metadadosResponse = await axios.get(
       `https://servicodados.ibge.gov.br/api/v4/malhas/estados/${idEstado}/metadados`
     );
-
     if (metadadosResponse.status !== 200) {
       throw new Error('Erro ao buscar metadados do estado');
     }
-
     const metadados = metadadosResponse.data[0];
-
     const estadoDetalhado = {
       sigla: estado.sigla,
       nome: estado.nome,
@@ -41,9 +33,7 @@ export default async function handler(req, res) {
       ibge_codigo: estado.id,
       area: metadados.area.dimensao || 'Não disponível',
     };
-
     return res.status(200).json(estadoDetalhado);
-
   } catch (err) {
     if (err instanceof BaseError) {
       return res.status(404).json({ error: err.message });
