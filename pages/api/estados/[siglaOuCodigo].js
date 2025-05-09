c2b0659ae0cf1c26371f77553d57a7ff75a403e8
Louie <cloud9.alice@gmail.com>
2025-05-09 08:31:43 -0300

    update

--- pages/api/estados/[siglaOuCodigo].js
+++ pages/api/estados/[siglaOuCodigo].js
@@ -8,7 +8,7 @@ export default async function handler(req, res) {
   const { siglaOuCodigo } = req.query;
 
   try {
-    console.log('Sigla ou Código recebido:', siglaOuCodigo);
+    // console.log('Sigla ou Código recebido:', siglaOuCodigo);
 
     if (!siglaOuCodigo || !CODIGOS_ESTADOS[siglaOuCodigo.toUpperCase()]) {
       throw new NotFoundError({
@@ -18,7 +18,12 @@ export default async function handler(req, res) {
     }
 
     const response = await getUfByCode(siglaOuCodigo.toUpperCase());
-    console.log('Resposta da API IBGE (getUfByCode):', response.status, response.data);
+
+    console.log(
+      'Resposta da API IBGE (getUfByCode):',
+      response.status,
+      response.data
+    );
 
     if (response.status !== 200 || !response.data) {
       throw new Error('Erro na requisição à API do IBGE');
@@ -30,9 +35,17 @@ export default async function handler(req, res) {
     const metadadosResponse = await axios.get(
       `https://servicodados.ibge.gov.br/api/v4/malhas/estados/${idEstado}/metadados`
     );
-    console.log('Resposta da API IBGE (metadados):', metadadosResponse.status, metadadosResponse.data);
 
-    if (metadadosResponse.status !== 200 || !Array.isArray(metadadosResponse.data)) {
+    console.log(
+      'Resposta da API IBGE (metadados):',
+      metadadosResponse.status,
+      metadadosResponse.data
+    );
+
+    if (
+      metadadosResponse.status !== 200 ||
+      !Array.isArray(metadadosResponse.data)
+    ) {
       throw new Error('Metadados do estado não encontrados');
     }
 
@@ -51,6 +64,8 @@ export default async function handler(req, res) {
     if (err instanceof BaseError) {
       return res.status(404).json({ error: err.message });
     }
-    return res.status(500).json({ error: err.message || 'Erro ao buscar estado.' });
+    return res
+      .status(500)
+      .json({ error: err.message || 'Erro ao buscar estado.' });
   }
 }
