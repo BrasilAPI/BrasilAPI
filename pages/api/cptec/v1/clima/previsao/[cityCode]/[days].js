ddd8ec31ca6ab1d749a8fb249403b9831d6e502f
Felipe Jovino <79649732+Jovinull@users.noreply.github.com>
2026-08-07 16:54:54 -0300

    fix(cptec): valida days inteiro em /clima/previsao/{cityCode}/{days} (#810)

--- pages/api/cptec/v1/clima/previsao/[cityCode]/[days].js
+++ pages/api/cptec/v1/clima/previsao/[cityCode]/[days].js
@@ -17,6 +17,14 @@ const action = async (request, response) => {
     });
   }
 
+  if (!Number.isInteger(Number(days))) {
+    throw new BadRequestError({
+      message: 'Quantidade de dias inválida, informe um valor inteiro',
+      type: 'request_error',
+      name: 'INVALID_NUMBER_OF_DAYS',
+    });
+  }
+
   if (days < MIN_DAYS || days > MAX_WEATHER_DAYS) {
     throw new BadRequestError({
       message: 'Quantidade de dias inválida (mínimo 1 dia e máximo 14 dias)',
